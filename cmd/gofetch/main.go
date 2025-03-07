46ad2098728ebc9c81a36aa4e6af8e9183493dbb
Amirali Amirifar <amirali.amirifar@gmail.com>
2025-03-07 23:07:05 +0330

    add nested view for downloads

--- cmd/gofetch/main.go
+++ cmd/gofetch/main.go
@@ -3,16 +3,10 @@ package main
 import (
 	"fmt"
 	"github.com/Amirali-Amirifar/gofetch.git/internal/tui"
-	tea "github.com/charmbracelet/bubbletea"
-	"os"
 )
 
 func main() {
-	tabs := []string{"New Download", "Downloads List", "Queues List"}
-	tabContent := []string{"Lip Gloss Tab", "Blush Tab", "Eye Shadow Tab", "Mascara Tab", "Foundation Tab"}
-	m := tui.Model{Tabs: tabs, TabContent: tabContent}
-	if _, err := tea.NewProgram(m).Run(); err != nil {
-		fmt.Println("Error running program:", err)
-		os.Exit(1)
+	if _, err := tui.GetTui().Run(); err != nil {
+		fmt.Println(err)
 	}
 }
--- internal/tui/tui.go
+++ internal/tui/tui.go
@@ -1,28 +1,37 @@
 package tui
 
 import (
+	"github.com/Amirali-Amirifar/gofetch.git/internal/tui/views"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
 	"strings"
 )
 
-type Model struct {
-	Tabs       []string
-	TabContent []string
-	activeTab  int
-	width      int
-	height     int
+type model struct {
+	Tabs      []string
+	children  []tea.Model
+	activeTab int
+	isFocused bool
+
+	// Terminal size
+	width  int
+	height int
 }
 
-func (m Model) Init() tea.Cmd {
-	return nil
+func (m model) Init() tea.Cmd {
+	return m.children[0].Init()
 }
 
-func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var cmd tea.Cmd
+
+	m.children[m.activeTab], _ = m.children[m.activeTab].Update(msg)
+
 	switch msg := msg.(type) {
 	case tea.WindowSizeMsg:
 		m.width = msg.Width
 		m.height = msg.Height
+
 	case tea.KeyMsg:
 		switch keypress := msg.String(); keypress {
 		case "ctrl+c", "q":
@@ -36,10 +45,10 @@ func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		}
 	}
 
-	return m, nil
+	return m, cmd
 }
 
-func TabBorderWithBottom(left, middle, right string) lipgloss.Border {
+func tabBorderWithBottom(left, middle, right string) lipgloss.Border {
 	border := lipgloss.RoundedBorder()
 	border.BottomLeft = left
 	border.Bottom = middle
@@ -48,25 +57,26 @@ func TabBorderWithBottom(left, middle, right string) lipgloss.Border {
 }
 
 var (
-	inactiveTabBorder = TabBorderWithBottom("┴", "─", "┴")
-	activeTabBorder   = TabBorderWithBottom("┘", " ", "└")
+	inactiveTabBorder = tabBorderWithBottom("┴", "─", "┴")
+	activeTabBorder   = tabBorderWithBottom("┘", " ", "└")
 	docStyle          = lipgloss.NewStyle().Padding(1, 2, 1, 2)
-	highlightColor    = lipgloss.AdaptiveColor{Light: "#874BFD", Dark: "#7D56F4"}
+	highlightColor    = lipgloss.Color("#7D56F4")
 	inactiveTabStyle  = lipgloss.NewStyle().Border(inactiveTabBorder, true).BorderForeground(highlightColor).Padding(0, 1)
 	activeTabStyle    = inactiveTabStyle.Border(activeTabBorder, true)
 	windowStyle       = lipgloss.NewStyle().
 				BorderForeground(highlightColor).
-				Padding(2, 0).
+				Padding(3, 3).
 				Align(lipgloss.Center).
 				Border(lipgloss.NormalBorder()).
 				UnsetBorderTop()
 )
 
-func (m Model) View() string {
+func (m model) View() string {
 	doc := strings.Builder{}
 
 	var renderedTabs []string
 
+	tabBarWidth := m.width
 	for i, t := range m.Tabs {
 		var style lipgloss.Style
 		isFirst, isLast, isActive := i == 0, i == len(m.Tabs)-1, i == m.activeTab
@@ -75,27 +85,44 @@ func (m Model) View() string {
 		} else {
 			style = inactiveTabStyle
 		}
+
 		border, _, _, _, _ := style.GetBorder()
 		if isFirst && isActive {
 			border.BottomLeft = "│"
 		} else if isFirst && !isActive {
 			border.BottomLeft = "├"
 		} else if isLast && isActive {
-			border.BottomRight = "│"
-		} else if isLast && !isActive {
-			border.BottomRight = "┤"
+			border.BottomRight = "└"
 		}
-		style = style.Border(border).Width(m.width / 5)
-		renderedTabs = append(renderedTabs, style.Render(t))
+
+		style = style.Width(20).Border(border)
+		renderedText := style.Render(t)
+
+		renderedTabs = append(renderedTabs, renderedText)
+		tabBarWidth = tabBarWidth - lipgloss.Width(renderedText)
 	}
 
+	blankBorder := lipgloss.HiddenBorder()
+	blankBorder.Bottom = "─"
+	blankBorder.BottomLeft = "─"
+	blankBorder.BottomRight = "┐"
+	blankTab := lipgloss.NewStyle().
+		Width(tabBarWidth - windowStyle.GetHorizontalFrameSize()).
+		Border(blankBorder).
+		BorderForeground(highlightColor).
+		Render("")
+
+	renderedTabs = append(renderedTabs, blankTab)
 	row := lipgloss.JoinHorizontal(lipgloss.Top, renderedTabs...)
+
+	tabContents := windowStyle.Width(
+		m.width - windowStyle.GetHorizontalFrameSize()).Height(m.height - windowStyle.GetVerticalFrameSize()).
+		// Background is set for debugging
+		Background(lipgloss.Color("#111")).
+		Render(m.children[m.activeTab].View())
+
 	doc.WriteString(row)
 	doc.WriteString("\n")
-	tabContents := windowStyle.Width(
-		lipgloss.Width(row) - windowStyle.GetHorizontalFrameSize()).Height(m.height - 6).
-		Render(m.TabContent[m.activeTab])
-
 	doc.WriteString(tabContents)
 
 	return lipgloss.Place(m.width, m.height,
@@ -103,6 +130,13 @@ func (m Model) View() string {
 		docStyle.Render(doc.String()))
 }
 
+func (m model) initializeChildren() tea.Model {
+	m.children = []tea.Model{nil, nil, nil}
+	m.children[0] = views.InitialModel()
+	m.children[0].Init()
+
+	return m
+}
 func max(a, b int) int {
 	if a > b {
 		return a
@@ -115,4 +149,11 @@ func min(a, b int) int {
 		return a
 	}
 	return b
+}
+
+func GetTui() *tea.Program {
+	tabs := []string{"New Download", "Downloads List", "Queues List"}
+	m := model{Tabs: tabs}.initializeChildren()
+
+	return tea.NewProgram(m)
 }
